ffc3d5fa354aed18d175085561f65043a67ff1ee
Grzegorz Macko <grzmacko@gmail.com>
2019-04-10 22:32:22 +0200

    Adapter for listview to main activity has been set.

--- BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/MainActivity.java
+++ BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/MainActivity.java
@@ -3,8 +3,14 @@ package com.yourstronghelper.grzegorzmacko.btcsignal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<Alert> dataModels;
+    AlertAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        listView=(ListView)findViewById(R.id.listView);
+        dataModels= new ArrayList<>();
+        Alert a1 = new Alert("bitmex", "USD", 6, 0 );
+        Alert a2 = new Alert("binance", "USD", 3, 1 );
+
+        adapter= new AlertAdapter(getApplicationContext(),dataModels);
+        listView.setAdapter(adapter);
     }
 }
