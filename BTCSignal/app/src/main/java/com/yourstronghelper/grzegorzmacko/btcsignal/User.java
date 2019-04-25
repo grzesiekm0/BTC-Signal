0700a1bee26ccfdf6d3671b339261432a721cd6d
Grzegorz Macko <grzmacko@gmail.com>
2019-04-25 22:49:39 +0200

    Model data for users has been created.

--- BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/User.java
+++ BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/User.java
@@ -1,4 +1,17 @@
 package com.yourstronghelper.grzegorzmacko.btcsignal;
 
 public class User {
+    private String numberPhone;
+
+    public User(String numberPhone) {
+        this.numberPhone = numberPhone;
+    }
+
+    public String getNumberPhone() {
+        return numberPhone;
+    }
+
+    public void setNumberPhone(String numberPhone) {
+        this.numberPhone = numberPhone;
+    }
 }
