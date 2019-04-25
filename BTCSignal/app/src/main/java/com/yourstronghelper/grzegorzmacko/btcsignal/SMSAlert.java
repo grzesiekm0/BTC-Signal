2cc590891f5ed9b120c83162aeaaae5b2ae68ee2
Grzegorz Macko <grzmacko@gmail.com>
2019-04-25 22:48:29 +0200

    Model data for webservice to sent sms has been created.

--- BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/SMSAlert.java
+++ BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/SMSAlert.java
@@ -1,4 +1,39 @@
 package com.yourstronghelper.grzegorzmacko.btcsignal;
 
-public class SMSAlert {
+import java.io.Serializable;
+
+public class SMSAlert implements Serializable {
+    private String phoneNumber;
+    private String course;
+    private int enableAlarm;
+
+    public SMSAlert(String phoneNumber, String course, int enableAlarm) {
+        this.phoneNumber = phoneNumber;
+        this.course = course;
+        this.enableAlarm = enableAlarm;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getEnableAlarm() {
+        return enableAlarm;
+    }
+
+    public void setEnableAlarm(int enableAlarm) {
+        this.enableAlarm = enableAlarm;
+    }
 }
