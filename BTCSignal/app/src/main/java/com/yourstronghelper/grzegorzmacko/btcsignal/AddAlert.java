1af2e14e75ce46631d13217af527045c88dff766
Grzegorz Macko <grzmacko@gmail.com>
2019-05-16 23:56:50 +0200

    An example implementation of the alert has been added.

--- BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/AddAlert.java
+++ BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/AddAlert.java
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AddAlert extends AppCompatActivity {
+    AlertDatabaseAdapter alertDatabaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alert);
+        alertDatabaseAdapter=new AlertDatabaseAdapter(getApplicationContext());
+        alertDatabaseAdapter=alertDatabaseAdapter.open();
+        //example data
+        Alert al = new Alert("bitmex", "PLN", "2000", 1);
+        System.out.println("Test"+al.getExchange());
+        alertDatabaseAdapter.insertEntry(al.getExchange(), al.getCurrency(), al.getCourse(), al.getEnableAlarm());
+        alertDatabaseAdapter.close();
     }
 }
