a25b2db9cea787afc63a5c8f787e11146597c540
Grzesiek Macko <grzmacko@gmail.com>
2019-06-10 13:58:07 +0200

    Toolbar view and test date has been created.  Toolbar has been added to main activity.

--- BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/MainActivity.java
+++ BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/MainActivity.java
@@ -3,6 +3,8 @@ package com.yourstronghelper.grzegorzmacko.btcsignal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,9 +19,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar was hanidng to view
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+//        //Toolbar was hanidng to view
+//        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        setSupportActionBar(myToolbar);
+
+        // Attaching the layout to the toolbar object
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        // Setting toolbar as the ActionBar with setSupportActionBar() call
+        setSupportActionBar(toolbar);
+
         //An instance database was initialized and a connection was established
         alertDatabaseAdapter=new AlertDatabaseAdapter(getApplicationContext());
         alertDatabaseAdapter=alertDatabaseAdapter.open();
@@ -34,5 +42,27 @@ public class MainActivity extends AppCompatActivity {
 
         adapter= new AlertAdapter(getApplicationContext(),dataModels);
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
