323eb4ec908bdec22c9f77d20f8005b3179e0fd0
Grzesiek Macko <grzmacko@gmail.com>
2019-06-18 09:42:55 +0200

    Tollbar with icons has been added to the adding activity.

--- BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/AddAlertActivity.java
+++ BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/AddAlertActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,13 +20,14 @@ public class AddAlertActivity extends AppCompatActivity implements AdapterView.O
     AlertDatabaseAdapter alertDatabaseAdapter;
     Button mAddButton;
     EditText mCourse;
+    Intent myIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alert);
         //Toolbar was hanidng to view
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(myToolbar);
 
         mAddButton = (Button) findViewById(R.id.add);
@@ -71,6 +74,30 @@ public class AddAlertActivity extends AppCompatActivity implements AdapterView.O
                 alertDatabaseAdapter.close();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add) {
+//            myIntent = new Intent(MainActivity.this, AddAlertActivity.class);
+//            MainActivity.this.startActivity(myIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
