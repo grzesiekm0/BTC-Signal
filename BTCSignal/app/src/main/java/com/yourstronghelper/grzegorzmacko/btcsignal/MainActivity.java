f8f6588972f570b17eb7053acb1773cab129caf8
Grzesiek Macko <grzmacko@gmail.com>
2020-02-17 13:57:34 +0100

    Example SyncAdapter has been added to MainActivity.

--- BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/MainActivity.java
+++ BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/MainActivity.java
@@ -1,5 +1,7 @@
 package com.yourstronghelper.grzegorzmacko.btcsignal;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -60,6 +62,15 @@ public class MainActivity extends AppCompatActivity {
     Intent myIntent;
     Toolbar toolbar;
     TextView textView, mTextView;
+    // Constants
+    // The authority for the sync adapter's content provider
+    public static final String AUTHORITY = "com.example.android.datasync.provider";
+    // An account type, in the form of a domain name
+    public static final String ACCOUNT_TYPE = "example.com";
+    // The account name
+    public static final String ACCOUNT = "dummyaccount";
+    // Instance fields
+    Account mAccount;
 
     static Context context;
 
@@ -86,6 +97,40 @@ public class MainActivity extends AppCompatActivity {
         }
         mTextView = (TextView) findViewById(R.id.textView);
 
+        // Create the dummy account
+        mAccount = CreateSyncAccount(this);
+    }
+
+    /**
+     * Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account CreateSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call context.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
     }
 
     //Sending request to the API server.
