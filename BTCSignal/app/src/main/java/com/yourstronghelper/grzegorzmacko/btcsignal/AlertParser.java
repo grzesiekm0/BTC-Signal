169f071795c4af5efd8b7679a08c1de3252b6d19
Grzesiek Macko <grzmacko@gmail.com>
2020-03-02 15:26:29 +0100

    Parse class for Json has been completed.

--- BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/AlertParser.java
+++ BTCSignal/app/src/main/java/com/yourstronghelper/grzegorzmacko/btcsignal/AlertParser.java
@@ -1,4 +1,18 @@
 package com.yourstronghelper.grzegorzmacko.btcsignal;
 
+import org.json.JSONObject;
+
+/**
+ * This is an example parser to 'parse' pretend json news feed.
+ */
 public class AlertParser {
+    public static Alert parse(JSONObject jsonArticle) {
+        Alert alert = new Alert();
+        alert.setAlertId(jsonArticle.optString("id"));
+        alert.setExchange(jsonArticle.optString("exchange"));
+        alert.setCurrency(jsonArticle.optString("currency"));
+        alert.setCourse(jsonArticle.optString("course"));
+        alert.setEnableAlarm(jsonArticle.optInt("enableAlarm"));
+        return alert;
+    }
 }
